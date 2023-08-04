ba97bc81b711b403d684b581d1fd8a1da16cbc97
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-08-04 23:02:05 +0530

    Q -17  phone number letter combination using java

--- String Problems/Q17_phn_num_letter_comb.java
+++ String Problems/Q17_phn_num_letter_comb.java
@@ -26,6 +26,28 @@
 // 0 <= digits.length <= 4
 // digits[i] is a digit in the range ['2', '9'].
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return Collections.emptyList();
 
+        String[] phone_map = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> output = new ArrayList<>();
+        backtrack("", digits, phone_map, output);
+        return output;
+    }
 
+    private void backtrack(String combination, String next_digits, String[] phone_map, List<String> output) {
+        if (next_digits.isEmpty()) {
+            output.add(combination);
+        } else {
+            String letters = phone_map[next_digits.charAt(0) - '2'];
+            for (char letter : letters.toCharArray()) {
+                backtrack(combination + letter, next_digits.substring(1), phone_map, output);
+            }
+        }
+    }
+}
