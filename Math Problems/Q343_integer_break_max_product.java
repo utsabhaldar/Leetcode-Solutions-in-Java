577dcf9d028a916c610c7d7ec039758400368528
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-10-06 10:14:55 +0530

    Q343. Integer Break Maximum Product

---
+++ Math Problems/Q343_integer_break_max_product.java
@@ -0,0 +1,27 @@
+// 343. Integer Break
+
+// Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+// Return the maximum product you can get.
+// Example 1:
+// Input: n = 2
+// Output: 1
+// Explanation: 2 = 1 + 1, 1 × 1 = 1.
+// Example 2:
+// Input: n = 10
+// Output: 36
+// Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+// Constraints:
+// 2 <= n <= 58
+
+
+// Time:O(1)
+// Space:O(1)
+public class Q343_integer_break_max_product {
+    public int integerBreak(int n) {
+        if(n==2) return 1;
+        else if(n==3) return 2;
+        else if(n%3==0) return (int) Math.pow(3, n/3);
+        else if(n%3==1) return 2*2* (int) Math.pow(3, (n-4)/3);
+        else return 2* (int) Math.pow(3, n/3);
+    }
+}
