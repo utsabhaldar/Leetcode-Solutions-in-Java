cb1d23683fe6f18bb2ef66a76a7a50a540ed278a
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-10-13 18:31:45 +0530

    Q2. Add Two Numbers

--- Linked List/Q2_add_2_nums_LL.java
+++ Linked List/Q2_add_2_nums_LL.java
@@ -27,7 +27,13 @@
 // Space Complexity: O(max(m,n))
 public class Q2_add_2_nums_LL {
 
-    /*
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
@@ -51,6 +57,4 @@ public class Q2_add_2_nums_LL {
         }
         return dummy.next;
     }
-
-    */
 }
