a651cd9529be4df2755f1b2cc897c54130207e64
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-11-07 07:33:01 +0530

    Q412. Fizz Buzz

---
+++ String Problems/Q412_fizz_buzz.java
@@ -0,0 +1,34 @@
+// 412. Fizz Buzz
+
+// Given an integer n, return a string array answer (1-indexed) where:
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+// Example 1:
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+// Constraints:
+// 1 <= n <= 104
+
+
+
+// Time:O(n)
+// Space:O(n)
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q412_fizz_buzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+
+        for(int i=1; i<=n; i++){
+            if(i%3 == 0 && i%5 == 0) ans.add("FizzBuzz");
+            else if(i%3 == 0) ans.add("Fizz");
+            else if(i%5 == 0) ans.add("Buzz");
+            else ans.add(String.valueOf(i));
+        }
+
+        return ans;
+    }
+}
